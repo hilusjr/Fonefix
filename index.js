7f66ddf5d4c78027341a9289e3e89620c353ffb6
Piotr Hilicki <pietrzef@gmail.com>
2021-11-27 08:40:28 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
+const navbar = document.querySelector(".navbar")
 const navbarButtons = document.querySelectorAll(".flex-container div")
+const navbarButton = document.querySelector(".flex-container > div:nth-child(7)")
+const menuButton = document.querySelector(".nav-btn")
+const logo = document.querySelector(".logo")
 
-navbarButtons.forEach(button => {
+// SCROLL TO ELEMENT
+navbarButtons.forEach((button) => {
     const buttonText = button.textContent
     button.addEventListener("click", () => {
-        const element = document.querySelector("." + buttonText + "-container")
-        console.log(window.innerHeight / 8)
-        scrollTo(0, element.offsetTop - (window.innerHeight / 12.5))
+        const element = document.querySelector(`.${buttonText}-container`)
+        scrollTo(0, element.offsetTop - window.innerHeight / 12.5)
+        if (menuButton.style.display == "block") toggleMenu()
     })
 })
+
+// MOBILE MENU TOGGLE
+menuButton.addEventListener("click", () => {
+    toggleMenu()
+})
+
+function toggleMenu() {
+    if (navbarButton.style.display == "none") {
+        navbar.style.height = "50vmax"
+        navbarButtons.forEach(element => {
+            element.style.display = "block"
+        })
+    } else {
+        navbar.style.height = "8vmax"
+        navbarButtons.forEach(element => {
+            element.style.display = "none"
+        })
+        logo.style.display = "block"
+
+    }
+}
--- start.js
+++ start.js
@@ -1,10 +1,14 @@
-const elements = document.querySelectorAll(".slide-right")
+const elements = document.querySelectorAll(".slide-right, .slide-down")
 
 const observer = new IntersectionObserver(entries => {
 
     entries.forEach(entry => {
         if (entry.isIntersecting) {
-            entry.target.style.animation = "slideInToRight .4s ease-out .4s forwards"
+            if (entry.target.className == "slide-right") {
+                entry.target.style.animation = "slideInToRight .4s ease-out .4s forwards"
+            } else if (entry.target.className == "slide-down") {
+                entry.target.style.animation = "slideInToBottom .4s ease-out .8s forwards"
+            }
         } else {
             entry.target.style.animation = "none"
         }
